233a4d3459f033af43a09f61b360d19ba64bc4df
vv <u0NEEp9v>
2016-11-22 18:20:33 +0400

    From the lesson

--- app/src/main/java/ru/samsung/itschool/game18/MyDraw.java
+++ app/src/main/java/ru/samsung/itschool/game18/MyDraw.java
@@ -7,21 +7,22 @@ import android.view.View;
 
 public class MyDraw extends View{
 
-	Vector v;
-	Square s;
+	Figure[] f = new Figure[100];
 	public MyDraw(Context context) {
 		super(context);
-		v = new Vector (100, 200);
-		s = new Square (200, 150);
+		f[0] = new Square(50, 30);
+		f[1] = new Circle(40, 100);
 	}
-
 	Paint paint = new Paint();
 	@Override
 	protected void onDraw(Canvas canvas) {
-		canvas.drawLine(0, 0, v.x, v.y, paint);
-		canvas.drawText(v.toString(), 50, 50, paint);
-		s.draw(canvas);
-		s.move();
+		for (Figure fig : f)
+		{
+			if (fig != null){
+				fig.draw(canvas);
+				fig.move();
+			}
+		}
 		invalidate();
 	}
 	
--- app/src/main/java/ru/samsung/itschool/game18/Square.java
+++ app/src/main/java/ru/samsung/itschool/game18/Square.java
@@ -3,25 +3,14 @@ package ru.samsung.itschool.game18;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-public class Square {
-    Vector pos;
-    Vector velocity;
-    Paint paint;
-
-    // Square(Vector pos)
-    // Square(Vector pos, Vector velocity)
-    Square(float posX, float posY) {
-        pos = new Vector(posX, posY);
-        paint = new Paint();
-        velocity = new Vector(0.1f, 0.1f);
+public class Square extends Figure{
+    int size = 100;
+    public Square(float posX, float posY) {
+        super(posX, posY);
     }
 
+    @Override
     void draw(Canvas canvas) {
-        canvas.drawRect(pos.x, pos.y, pos.x + 50, pos.y + 50, paint);
+        canvas.drawRect(pos.x, pos.y, pos.x + size, pos.y + size, paint);
     }
-
-    void move() {
-        pos.sum(velocity);
-    }
-
 }
